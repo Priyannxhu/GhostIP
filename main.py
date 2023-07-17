5408d28be9e933bff1439f05c75dd0dd3a482113
GhostLinux <111190070+GhostLinuxx@users.noreply.github.com>
2023-07-16 22:38:13 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import pyfiglet
+
+def geolocate_ip(ip):
+    url = f"https://ipapi.co/{ip}/json/"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        country = data.get('country_name', 'Unknown')
+        city = data.get('city', 'Unknown')
+        region = data.get('region', 'Unknown')
+        postal = data.get('postal', 'Unknown')
+        latitude = data.get('latitude', 'Unknown')
+        longitude = data.get('longitude', 'Unknown')
+
+        figlet = pyfiglet.Figlet()
+        print(figlet.renderText("Ghost IP"))
+        print(f"IP: {ip}")
+        print(f"Country: {country}")
+        print(f"City: {city}")
+        print(f"Region: {region}")
+        print(f"Postal Code: {postal}")
+        print(f"Latitude: {latitude}")
+        print(f"Longitude: {longitude}")
+    else:
+        print("Unable to geolocate IP.")
+
+# Example usage
+ip_address = input("Enter IP address to geolocate: ")
+geolocate_ip(ip_address)
